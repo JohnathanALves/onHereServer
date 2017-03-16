8158c80ebf5046d988a8d4648bb5fc2b5e4f8f4f
Johnathan alves <Johnathan alves>
2017-03-15 21:51:50 -0300

    Eventos de conexao ao BD

--- app.js
+++ app.js
@@ -8,6 +8,22 @@ var express     = require('express')
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://admin:admin@ds113660.mlab.com:13660/onheredb');
 
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose default connection openned ');
+});
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose default connection disconnected');
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
