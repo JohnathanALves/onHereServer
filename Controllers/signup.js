33eb0ec2e9a3864bc25014ca96798653a9afe308
Johnathan alves <Johnathan alves>
2017-03-23 16:24:03 -0300

    correcao de bug no cadastro de novo usuario

--- Controllers/signup.js
+++ Controllers/signup.js
@@ -6,36 +6,31 @@ module.exports = function(req, res, next){
 	var pass 	= req.body.password;
 	var name 	=	req.body.fullname;
 
-	Model.findOne({email: email}, function(err, docs){
-		if(!err){
+	Model.findOne({email: email}, function(err, user){
+		if(err){
 			console.log('Error in signup: '+err);
-			return res.json(status: '418');
+			return res.json({status: '418'});
 		}
-		if(docs) {
+		if(user) {
 			console.log('Email ja cadastrado: '+email);
-			return res.json('408');
-		} else {
-			//criacao do objeto
-			var data = new Model({
-		    email: email,
-		    password: createHash(pass),
-		    fullname: name
-			  });
-				// salva o objeto
-			  data.save(function(err) {
-			    if (err){
-						console.log('Error in signup: '+err);
-						return res.json(status: '418');
-		      } else{
-		        console.log('Usuario criado' + data.email);
-		        res.json({
-							status: '409'
-						 });
-		      }
-			  });
+			return res.json({status: '408'});
 		}
+		//criacao do objeto
+		var data = new Model({
+	    email: email,
+	    password: createHash(pass),
+	    fullname: name
+		  });
+			// salva o objeto
+		  data.save(function(err, user){
+		    if (err){
+					console.log('Error in signup: '+err);
+					return res.json({status: '418'});
+	      }
+	        console.log('Usuario criado' + data.email);
+	        res.json({status: '409'});
+		  });
 	});
-
 };
 var createHash = function(password){
   return bCrypt.hashSync(password, bCrypt.genSaltSync(5), null);
