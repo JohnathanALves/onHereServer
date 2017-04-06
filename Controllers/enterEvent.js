ca3a355ba3710327086f994591ff156a671c0539
Johnathan alves <Johnathan alves>
2017-04-06 09:32:45 -0300

    bug entrada de evento

--- Controllers/enterEvent.js
+++ Controllers/enterEvent.js
@@ -4,27 +4,26 @@ var geolib = require('geolib');
 module.exports = function(req, res, next){
   var id = req.query.id;
   var usermail = req.user.email;
-  var posicao = {latitude: req.user.latitude, longitude: req.user.longitude};
-  var _idEvento;
-  var localizaEvento = {};
+  //var posicao = {latitude: req.user.latitude, longitude: req.user.longitude};
+  var chave;
+  if(typeof id === 'undefined' || id == null){
+    return res.json({status: '414'});
+  }
+  //var localizaEvento = {};
   Evento.findOne({chave: id}, function(err, evento){
     if (err){
-      res.json(status: '414');
+      return res.json({status: '414'});
     }
-    _idEvento = evento._id;
-    localizaEvento = {latitude: evento.latitude, longitude: evento.longitude};
-    var distancia = geolib.getDistance(posicao, localizaEvento, 1, 2);
-    if (distancia > 16){
-      res.json(status: '419');
+    if(!evento){
+      return res.json({status: '414'});
     }
+    var part = [];
+    part = evento.participantes;
+    part.push(usermail);
+    evento.participantes = part;
+    evento.save(function(err, event){
+      if(err) return res.json({status: '414'});
+      return res.json({status: '417'});
+    });
   });
-  Evento.findByIdAndUpdate(
-    _idEvento,
-    {$push: {"participantes": usermail}},
-    {safe: true, upsert: true},
-    function(err, model){
-      if(err) return res.json(err);
-      return res.json(status: '417');
-    }
-  );
 };
